7b69d053df9c4217f85e27b773de5d5c58525f40
MarkMokhles3 <mark.mokhles1@gmail.com>
2024-08-29 17:52:08 +0300

    Add intro Screen

---
+++ KhaznaTask/IntroScreen/Router/IntroScreenRouterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  IntroScreenRouterProtocol.swift
+//  KhaznaTask
+//
+//  Created by Mark Shafik on 29/08/2024.
+//
+
+import Foundation
+
+protocol IntroScreenRouterProtocol: AnyObject {
+    func navigateToPostsListScreen()
+}
---
+++ KhaznaTask/Repo/PostsAPIServiceProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  PostsAPIServiceProtocol.swift
+//  KhaznaTask
+//
+//  Created by Mark Shafik on 27/08/2024.
+//
+
+import Foundation
+
+protocol PostsAPIServiceProtocol{
+    func fetchPosts(page: Int, completion: @escaping (Result<[Post], Error>) -> Void) 
+}
+
+struct Post: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
